33b43cad7aafb09f99bf9ce6790fed0434ffe1d1
Matrixoid <mr.art.pro@mail.ru>
2026-08-19 23:22:40 +0400

    feat: Task-008. Add harvesting results to JobStatusResponse

--- DataHarvester.Orchestrator/Features/Jobs/GetJobStatus/GetJobStatusQueryHandler.cs
+++ DataHarvester.Orchestrator/Features/Jobs/GetJobStatus/GetJobStatusQueryHandler.cs
@@ -3,15 +3,28 @@ using MediatR;
 
 namespace DataHarvester.Orchestrator.Features.Jobs.GetJobStatus
 {
+    /// <summary>
+    /// Обработчик запроса получения информации по задаче.
+    /// </summary>
     public class GetJobStatusQueryHandler : IRequestHandler<GetJobStatusQuery, JobStatusResponse?>
     {
         private readonly IHarvestingJobRepository _repository;
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса заданным значением <paramref name="repository"/>.
+        /// </summary>
+        /// <param name="repository">Репозиторий, в котором хранятся заведённые задачи.</param>
         public GetJobStatusQueryHandler(IHarvestingJobRepository repository)
         {
             _repository = repository;
         }
 
+        /// <summary>
+        /// Обработчик запроса получения информации по задаче.
+        /// </summary>
+        /// <param name="request">Запрос, который нужно обработать.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Ответ сервиса, в котором хранятся данные по задаче.</returns>
         public Task<JobStatusResponse?> Handle(GetJobStatusQuery request, CancellationToken cancellationToken)
         {
             var job = _repository.GetById(request.JobId);
@@ -19,7 +32,7 @@ namespace DataHarvester.Orchestrator.Features.Jobs.GetJobStatus
             if (job == null)
                 return Task.FromResult<JobStatusResponse?>(null);
 
-            var response = new JobStatusResponse(job.Id, job.Url, job.Status);
+            var response = new JobStatusResponse(job.Id, job.Url, job.Title, job.LinksCount, job.Status);
             return Task.FromResult<JobStatusResponse?>(response);
         }
     }
--- DataHarvester.Orchestrator/Features/Jobs/GetJobStatus/JobStatusResponse.cs
+++ DataHarvester.Orchestrator/Features/Jobs/GetJobStatus/JobStatusResponse.cs
@@ -7,6 +7,8 @@ namespace DataHarvester.Orchestrator.Features.Jobs.GetJobStatus
     /// </summary>
     /// <param name="JobId">Идентификатор задачи.</param>
     /// <param name="Url">URL адрес страницы, с которой собираются данные.</param>
+    /// <param name="Title">Название страницы, с которой происходил сбор данных.</param>
+    /// <param name="LinksCount">Число ссылок, которые были обнаружены на странице.</param>
     /// <param name="Status">Текущий статус выполнения задачи.</param>
-    public record JobStatusResponse(Guid JobId, string Url, JobStatus Status);
+    public record JobStatusResponse(Guid JobId, string Url, string? Title, int? LinksCount, JobStatus Status);
 }
